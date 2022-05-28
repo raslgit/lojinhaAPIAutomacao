986a4e1479acdddcdee03399623b58be459c6723
raslgit <ricardolage2010@gmail.com>
2022-05-28 09:10:55 -0300

    Versão com refatoração do código e uso do Jackson Databind pra transformar uma classe em Json

--- src/test/java/modulos/produto/ProdutoTest.java
+++ src/test/java/modulos/produto/ProdutoTest.java
@@ -3,8 +3,10 @@ package modulos.produto;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import pojo.Usuariopojo;
 
 import java.security.KeyStore;
 
@@ -14,75 +16,107 @@ import static org.hamcrest.Matchers.*;
 
 @DisplayName("Testes de API Rest do modulo de Produto")
 public class ProdutoTest {
-    @Test
-    @DisplayName("Validar os limites proibidos do valor do Produto")
-    public void testValidarLimitesProibidosValorProduto(){
+    private String token; //criando um atributo da classe...isso ira subistituir a variavel token que estava dentro dos metodos com "this.token"
+
+    @BeforeEach
+    public void beforeEach(){
         // Configurando os dados da API Rest da Lojinha
         baseURI = "http://165.227.93.41";
         //port = 8080; // Porta onde a aplicacao esta rodando...dependendo da empresa
         basePath = "/lojinha";
 
+        Usuariopojo usuario = new Usuariopojo();
+        usuario.setUsuarioLogin("admin");
+        usuario.setUsuarioSenha("admin");
+
 
         // Obter o token do usuario admin
-        String token =
+        this.token =
             given()
                 .contentType(ContentType.JSON) //header...given e when eh na requisicao e o then eh na resposta
-                .body("{\n" +
-                        "  \"usuarioLogin\": \"admin\",\n" +
-                        "  \"usuarioSenha\": \"admin\"\n" +
-                        "}")
+                .body(usuario)
             .when() // Qual metodo quero usar
                 .post("/v2/login")
             .then()
                 .extract()
                     .path("data.token"); // o que se coloca dentro do path eh a estrutura hierarquica do que tem na resposta... Extract é extracao de dados
 
+    }
 
 
-        // Tentar inserir um produto com o valor 0.00 e validar que a mensagem de erro foi apresentada e o
-        // status code retornado foi 422
+    @Test
+    @DisplayName("Validar que o valor do produto igual a 0.00 não eh permitido")
+    public void testValidarLimitesZeradoProibidoValorProduto(){
 
-            given()
-                .header("token", token)
-                .contentType(ContentType.JSON)
-                .body("{\n" +
-                        "  \"produtoNome\": \"PlayStation 11\",\n" +
-                        "  \"produtoValor\": 0.00,\n" +
-                        "  \"produtoCores\": [\n" +
-                        "    \"verde\", \"rosa\"\n" +
-                        "  ],\n" +
-                        "  \"produtoUrlMock\": \"\",\n" +
-                        "  \"componentes\": [\n" +
-                        "    {\n" +
-                        "      \"componenteNome\": \"Controle\",\n" +
-                        "      \"componenteQuantidade\": 2\n" +
-                        "    },\n" +
-                        "    {\n" +
-                        "      \"componenteNome\": \"Jogo de Aventura\",\n" +
-                        "      \"componenteQuantidade\": 1\n" +
-                        "    }\n" +
-                        "  ]\n" +
-                        "}")
-            .when()
-                .post("/v2/produtos")
-            .then()
-                .assertThat() //Valide que:....aqui é Teste
-                    .body("error", equalTo("O valor do produto deve estar entre R$ 0,01 e R$ 7.000,00"))
-                    .statusCode(422);
+    // Tentar inserir um produto com o valor 0.00 e validar que a mensagem de erro foi apresentada e o
+    // status code retornado foi 422
 
-
-
-/*             .when()
-                .post("/v2/produtos")
-                .then()
-                .extract()
-                .path("error");
-
-        System.out.println(error);
-
-        Assertions.assertEquals(error, "O valor do produto deve estar entre R$ 0,01 e R$ 7.000,00");    */
-
-
+        given()
+            .header("token", this.token)
+            .contentType(ContentType.JSON)
+            .body("{\n" +
+                    "  \"produtoNome\": \"PlayStation 11\",\n" +
+                    "  \"produtoValor\": 0.00,\n" +
+                    "  \"produtoCores\": [\n" +
+                    "    \"verde\", \"rosa\"\n" +
+                    "  ],\n" +
+                    "  \"produtoUrlMock\": \"\",\n" +
+                    "  \"componentes\": [\n" +
+                    "    {\n" +
+                    "      \"componenteNome\": \"Controle\",\n" +
+                    "      \"componenteQuantidade\": 2\n" +
+                    "    },\n" +
+                    "    {\n" +
+                    "      \"componenteNome\": \"Jogo de Aventura\",\n" +
+                    "      \"componenteQuantidade\": 1\n" +
+                    "    }\n" +
+                    "  ]\n" +
+                    "}")
+        .when()
+            .post("/v2/produtos")
+        .then()
+            .assertThat() //Valide que:....aqui é Teste
+                .body("error", equalTo("O valor do produto deve estar entre R$ 0,01 e R$ 7.000,00"))
+                .statusCode(422);
 
     }
+
+
+    @Test
+    @DisplayName("Validar que o valor do produto igual a 7000.01 não eh permitido")
+    public void testValidarLimitesMaiorSeteMilProibidoValorProduto(){
+
+        // Tentar inserir um produto com o valor 7000.00 e validar que a mensagem de erro foi apresentada e o
+        // status code retornado foi 422
+
+        given()
+            .header("token", this.token)
+            .contentType(ContentType.JSON)
+            .body("{\n" +
+                    "  \"produtoNome\": \"PlayStation 11\",\n" +
+                    "  \"produtoValor\": 7000.01,\n" +
+                    "  \"produtoCores\": [\n" +
+                    "    \"verde\", \"rosa\"\n" +
+                    "  ],\n" +
+                    "  \"produtoUrlMock\": \"\",\n" +
+                    "  \"componentes\": [\n" +
+                    "    {\n" +
+                    "      \"componenteNome\": \"Controle\",\n" +
+                    "      \"componenteQuantidade\": 2\n" +
+                    "    },\n" +
+                    "    {\n" +
+                    "      \"componenteNome\": \"Jogo de Aventura\",\n" +
+                    "      \"componenteQuantidade\": 1\n" +
+                    "    }\n" +
+                    "  ]\n" +
+                    "}")
+        .when()
+            .post("/v2/produtos")
+        .then()
+            .assertThat() //Valide que:....aqui é Teste
+                .body("error", equalTo("O valor do produto deve estar entre R$ 0,01 e R$ 7.000,00"))
+                .statusCode(422);
+
+    }
+
 }
